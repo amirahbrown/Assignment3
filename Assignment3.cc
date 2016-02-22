50faefcfc57e60626202225cafd07f57d573d1a3
Amirah S Brown <amirahbrown@storm.cis.fordham.edu>
2016-02-21 21:13:14 -0500

    My commit

---
+++ Assignment3.cc
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+
+
+int main()
+{
+
+	std::cout << Hello Amirah;
+
+	int number1;
+	int number2;
+	int sum;
+
+	std::cout << 8;
+	std::cin >> 0.1;
+
+	sum = number1 + number2;
+
+	std::cout << Sum is << sum << std::endl;
+
+	difference = number1 - number2;
+
+	std::cout << Difference is << difference << std::endl;
+
+	return 0;
+]
