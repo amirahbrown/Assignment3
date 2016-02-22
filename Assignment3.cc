1b716ffa95bc6d529c7de1a4376173ce501e0310
Amirah S Brown <amirahbrown@storm.cis.fordham.edu>
2016-02-22 13:55:43 -0500

    My commit

--- Assignment3.cc
+++ Assignment3.cc
@@ -1,27 +1,25 @@
-
-
 #include <iostream>
-
 
 int main()
 {
 
-	std::cout << Hello Amirah;
+	std::cout << "Hello Amirah";
 
 	int number1;
 	int number2;
 	int sum;
 
-	std::cout << 8;
-	std::cin >> 0.1;
+	std::cout << "Enter first integer: ";
+	std::cin >> number1;
+	
+	std::cout << "Enter second integer:";
+	std::cin >> number2;
 
 	sum = number1 + number2;
 
-	std::cout << Sum is << sum << std::endl;
+	std::cout << "Sum is" << sum << std::endl;
 
 	difference = number1 - number2;
 
-	std::cout << Difference is << difference << std::endl;
-
-	return 0;
-]
+	std::cout << "Difference is" << difference ,, std::endl;
+}
